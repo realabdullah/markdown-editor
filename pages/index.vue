4c8abbbce024470c8dceb1cf3b158a5d0d37c219
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-06-24 23:36:03 +0100

    (chore): playground exhibit

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
+  <BaseHeader />
   <div>
     <h1>Home</h1>
+    <ButtonsSave />
+    <br>
+    <ButtonsNewDoc />
+    <br>
+    <Toggle />
+    <br>
+    <FileRename />
   </div>
 </template>
